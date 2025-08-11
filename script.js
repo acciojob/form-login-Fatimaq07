dabc285654e6959dbc95d16fdd464363e1405986
FATIMA QURESHI <115686917+Fatimaq07@users.noreply.github.com>
2025-08-12 00:38:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
-    //Write your code here
+ function getFormvalue() {
+            let firstName = document.getElementById("fname").value.trim();
+            let lastName = document.getElementById("lname").value.trim();
 
-}
+            let fullName = firstName + " " + lastName;
+
+            alert(fullName);
+            return false;
+        }
